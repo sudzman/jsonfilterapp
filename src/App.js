d24a7733996df84cd1419f05572f9b5933d9d34f
sudzman <67327994+sudzman@users.noreply.github.com>
2021-08-25 19:49:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React, { useState } from 'react';
 import './App.css';
 import DataFetching from './DataFetching';
 
+
 function App() {
+
+
   return (
     <div className="container-fluid">
      <DataFetching/>
+
     </div>
   )
 }
+
 export default App;
